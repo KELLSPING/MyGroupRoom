cfcbb611d5d62a711e1398908de9bd3cac6bc357
Kells_P <kellsping@gmail.com>
2023-12-05 18:01:18 +0800

    Add view.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.mygrouproom"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mygrouproom"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
