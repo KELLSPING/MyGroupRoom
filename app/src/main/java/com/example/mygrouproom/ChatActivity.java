457a7e40ddd8d6eae34897310065f3cac33c068a
Kells_P <kellsping@gmail.com>
2023-12-06 20:08:02 +0800

    Add some "text_to_speech" code.

--- app/src/main/java/com/example/mygrouproom/ChatActivity.java
+++ app/src/main/java/com/example/mygrouproom/ChatActivity.java
@@ -2,6 +2,7 @@ package com.example.mygrouproom;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
@@ -35,7 +36,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity implements  TextToSpeech.OnInitListener {
     DatabaseReference userRef, groupRef, groupNameRef;
     FirebaseDatabase database;
     FirebaseAuth auth;
@@ -57,6 +58,8 @@ public class ChatActivity extends AppCompatActivity {
     ScrollView scrollView;
     String chatName, chatSenderId, chatSendMsgTimeStamp, chatMessage, chatImageUri, chatMsgKey;
 
+    private TextToSpeech tts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +77,9 @@ public class ChatActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
         getSupportActionBar().setTitle(database.getReference("Groups").child("GroupChatRoom1").getKey());
+
+        initTextToSpeech();
+        checkingSpeakTest();
 
         // get current group chat room name
         groupRef = database.getReference().child("Groups");
@@ -115,7 +121,7 @@ public class ChatActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // 讀取數據時發生錯誤
-                Log.e("Firebase", "Error reading data", databaseError.toException());
+                Log.d("kells", "Firebase Error reading data", databaseError.toException());
             }
         });
 
@@ -212,15 +218,15 @@ public class ChatActivity extends AppCompatActivity {
 
     } // onStart
 
-    private String formatTimeStamp(String chatSendMsgTimeStamp) {
+    private String formatTimeStamp(String timeStamp) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
 
-        long chatSendMsgTimeStampL = Long.parseLong(chatSendMsgTimeStamp);
+        long timeStampL = Long.parseLong(timeStamp);
 
-        Date date = new Date(chatSendMsgTimeStampL);
+        Date date = new Date(timeStampL);
 
-        String chatSendMsgTimeStampF = dateFormat.format(date);
-        return chatSendMsgTimeStampF;
+        String timeStampF = dateFormat.format(date);
+        return timeStampF;
     }
 
     @Override
@@ -239,10 +245,20 @@ public class ChatActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Log.d("kells", "ChatActivity : onStop()");
+
+        // 不管是否正在朗读TTS都被打断
+        tts.stop();
+        // 关闭，释放资源
+        tts.shutdown();
     }
 
     @Override
     protected void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
         super.onDestroy();
         Log.d("kells", "ChatActivity : onDestroy()");
     }
@@ -273,5 +289,96 @@ public class ChatActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onInit(int i) {
+        if (i == TextToSpeech.SUCCESS) {
+            // setLanguage设置语言
+            int result = tts.setLanguage(Locale.CHINA);
+            int result2 = tts.setLanguage(Locale.CHINESE);
+            Log.d("kells：", "语言设置结果："+result+":"+result2);
+            // TextToSpeech.LANG_MISSING_DATA：表示语言的数据丢失
+            // TextToSpeech.LANG_NOT_SUPPORTED：不支持
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.d("kells：", "数据丢失或不支持：");
+                Toast.makeText(this, "数据丢失或不支持", Toast.LENGTH_SHORT).show();
+            }else {
+                //不支持中文就将语言设置为英文
+                Log.d("kells：", "将语言设置为英文");
+                //tts.setLanguage(Locale.US);
+                //tts.setLanguage(Locale.CHINESE);
+                tts.setLanguage(Locale.CHINA);
+            }
+        }
+    }
+
+    private void initTextToSpeech() {
+        // 参数Context,TextToSpeech.OnInitListener
+        tts = new TextToSpeech(this, this);
+        // 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
+        tts.setPitch(1.0f);
+        // 设置语速
+//        tts.setSpeechRate(0.5f);
+
+    }
+
+    private void speakText(final String text, String Language) {
+
+        if (Language == "tw"){
+            Log.d("kells", "tw " );
+
+            tts.setLanguage(Locale.CHINA);
+        }else{
+            Log.d("kells", "en " );
+            tts.setLanguage(Locale.US);
+        }
+
+        Log.d("kells", "准备朗读" );
+        // validate
+        if (TextUtils.isEmpty(text)) {
+            Toast.makeText(ChatActivity.this, "请您输入要朗读的文字", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+        if (tts != null && !tts.isSpeaking()) {
+            /*
+                TextToSpeech的speak方法有两个重载。
+                // 执行朗读的方法
+                speak(CharSequence text,int queueMode,Bundle params,String utteranceId);
+                // 将朗读的的声音记录成音频文件
+                synthesizeToFile(CharSequence text,Bundle params,File file,String utteranceId);
+                第二个参数queueMode用于指定发音队列模式，两种模式选择
+                （1）TextToSpeech.QUEUE_FLUSH：该模式下在有新任务时候会清除当前语音任务，执行新的语音任务
+                （2）TextToSpeech.QUEUE_ADD：该模式下会把新的语音任务放到语音任务之后，
+                等前面的语音任务执行完了才会执行新的语音任务
+             */
+            Log.d("kells", "开始朗读");
+
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    } // speakText
+
+
+    private void checkingSpeakTest() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+
+                    speakText("语音设备检测结果正常。","zh-TW");
+                    Log.d("kells" ,"Joel");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
